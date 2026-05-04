1df3717fd2d47006648161467026add7494fc2b8
md-asik-reza-tasin <mdasikreza1997@gmail.com>
2026-05-04 21:48:33 +0600

    make surah pages static

--- quran-frontend/app/surah/[id]/page.tsx
+++ quran-frontend/app/surah/[id]/page.tsx
@@ -1,5 +1,11 @@
 import SurahReader from "../../../Components/SurahReader/SurahReader";
 
+export async function generateStaticParams() {
+  return Array.from({ length: 114 }, (_, i) => ({
+    id: String(i + 1),
+  }));
+}
+
 export default async function SurahPage({
   params,
 }: {
