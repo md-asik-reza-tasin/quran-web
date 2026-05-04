463ac32aff15899c0967e128bd853af6b0340ddf
md-asik-reza-tasin <mdasikreza1997@gmail.com>
2026-05-04 21:54:31 +0600

    redirect

--- quran-frontend/app/page.tsx
+++ quran-frontend/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import TopBar from "../Components/Home/TopBar";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+
+  redirect('/surah/1');
+
   return (
     <div>
       <TopBar />
